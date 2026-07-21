25ddcdee0adc1df33a56565075a5084626887c48
ehsan <ehsanalizadehkashtiban@gmail.com>
2026-07-22 03:36:37 +0900

    added comments

--- scripts/run_simulation.py
+++ scripts/run_simulation.py
@@ -12,7 +12,7 @@ def main():
         base_save_dir="training_data",
 
         # ── Dataset size ──────────────────────────────────────────────
-        n_samples=1900,
+        n_samples=20,
 
         # ── Ray parameters ────────────────────────────────────────────
         num_angles=6,
